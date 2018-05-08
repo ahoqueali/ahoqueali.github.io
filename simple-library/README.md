5c08e7328554759615dbcbaf3eadc05105b9df5f
Hoque Ali <hoque.ali@digital.cabinet-office.gov.uk>
2018-05-08 10:17:22 +0100

    Remove simple library

--- simple-library/README.md
+++
@@ -1,5 +0,0 @@
-== How to run the simple library system api tests ==
-
-Execute the following command:
-
-	./gradlew clean build test  
--- simple-library/src/main/java/de/berenberg/library/DVDImpl.java
+++
@@ -1,9 +0,0 @@
-package de.berenberg.library;
-
-public class DVDImpl extends AbstractTitleItem implements DVD{
-
-	public DVDImpl(String id) {
-		super(id);
-	}
-
-}
